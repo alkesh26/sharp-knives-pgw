236576ed2b03f4d77703850ba3ccbb48615ea86b
Swanand Pagnis <swanand.pagnis@gmail.com>
2020-09-16 15:37:13 +0530

    Update pip and pgcli packages

--- initial-setup.sh
+++ initial-setup.sh
@@ -2,7 +2,9 @@
 
 apt-get clean
 apt-get update
-apt-get install -y build-essential libreadline6 libreadline6-dev postgresql postgresql-contrib python-pip libpq-dev python-dev
-pip install pgcli
+apt-get install -y build-essential libreadline6 libreadline6-dev python-pip libpq-dev python-dev
+pip install --upgrade pip
+apt-get install -y postgresql postgresql-contrib
+pip install pgcli==2.2.0
 
 source ${SHARED_DIR}/scripts/pg-setup.sh
